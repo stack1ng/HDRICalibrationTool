7b2893eeeb43a51d60594a6d56e723fa70b5ecea
nil <tomeaton76@gmail.com>
2025-10-16 20:50:29 -0700

    cleaned up the vendored binary logic + switched the name

--- src-tauri/src/raw_image_help.rs
+++ src-tauri/src/raw_image_help.rs
@@ -8,8 +8,7 @@ use std::{
 use tauri_plugin_shell::ShellExt;
 
 use tauri::Manager;
-
-const VENDOR_DIR: &str = "vendor";
+use crate::vendored_binaries::vendored_working_dir;
 
 /// Converts raw image into .tiff image for front end use.
 #[tauri::command]
@@ -39,21 +38,7 @@ pub async fn convert_raw_img(
         return Err("Couldn't create new directory".to_string());
     }
 
-    let cur_exe = env::current_exe().unwrap().parent().unwrap().to_path_buf();
-
-    // Get working directory of libraw.dll (only really needed for windows)
-    let dcraw_emu_build_working_directory = if cfg!(target_os = "macos") {
-        if cfg!(debug_assertions) {
-            // macOS dev mode
-            cur_exe.join(VENDOR_DIR)
-        } else {
-            // macOS release mode (inside .app bundle)
-            cur_exe.join("../Resources/".to_string() + VENDOR_DIR)
-        }
-    } else {
-        // Linux and Windows
-        cur_exe.join(VENDOR_DIR)
-    };
+    let dcraw_emu_build_working_directory = vendored_working_dir();
 
     let mut cmd: Command;
     let mut tiffs: Vec<String> = Vec::new();
